8f543ed7691a125aefe834750da15af9f8f978da
Tim Lee <timlee@tims-mbp.lan>
2020-03-30 20:58:00 +0800

    fixed create_table

--- webMJ.py
+++ webMJ.py
@@ -7,18 +7,19 @@ def make_shell_context():
     return {'db': db, 'User': User}
 
 
-if __name__ == '__main__':
+db.create_all()
+if app.config['DEBUG']:
+    print(app.config['DEBUG'])
+    db.drop_all()
     db.create_all()
-    if app.config['DEBUG']:
-        db.drop_all()
-        testUsers = {
-            'username': ['alice', 'bob', 'candice', 'tim'],
-            'email': ['alice@example.com', 'bob@example.com', 'candice@example.com', 'tim@example.com'],
-            'password': ['abc', 'def', 'ghi', 'tim']
-        }
-        for i in range(len(testUsers['username'])):
-            u = User(username=testUsers['username'][i], email=testUsers['email'][i])
-            u.set_password(testUsers['password'][i])
-            db.session.add(u)
-            db.session.commit()
-    app.run()
+    testUsers = {
+        'username': ['alice', 'bob', 'candice', 'tim'],
+        'email': ['alice@example.com', 'bob@example.com', 'candice@example.com', 'tim@example.com'],
+        'password': ['abc', 'def', 'ghi', 'tim']
+    }
+    for i in range(len(testUsers['username'])):
+        u = User(username=testUsers['username'][i], email=testUsers['email'][i])
+        u.set_password(testUsers['password'][i])
+        db.session.add(u)
+        db.session.commit()
+app.run()
